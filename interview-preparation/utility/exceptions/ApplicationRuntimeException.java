1654d55b53d0524614c84577883f68bf56c714ba
Gauta Raj <Gautam.Raj@quintiles.com>
2021-06-03 19:15:51 +0530

    Created Java Based Learning Architecture

---
+++ interview-preparation/utility/exceptions/ApplicationRuntimeException.java
@@ -0,0 +1,9 @@
+package utility.exceptions;
+
+public class ApplicationRuntimeException extends RuntimeException {
+	private static final long serialVersionUID = -727894269200491017L;
+	
+	public ApplicationRuntimeException() {
+		super();
+	}	
+}
